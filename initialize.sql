27fe6ec05bce5bf7b9ac947f3fe7373c6b5cb0e2
Simon002-guo <junyug@andrew.cmu.edu>
2024-11-23 11:22:47 -0500

    Add files via upload

---
+++ initialize.sql
@@ -0,0 +1,12 @@
+-- drop the trax database if it exists
+DROP database if EXISTS trax;
+
+-- create it afresh
+CREATE database trax;
+\c trax
+\i create.SQL
+
+-- load the data
+\copy "User"(UserID,Username,email) FROM './User.csv' csv header;
+\copy Channel(ChannelID, Channelname, Permission, Rank) FROM './Channel.csv' csv header;
+\copy Channel_Message(Message,Time,UserID,ChannelID) FROM './Channel_Message.csv' csv header;
