29d4bd01dabb3c4a90f70097a803cc38d3a37433
Simon002-guo <junyug@andrew.cmu.edu>
2024-11-22 19:55:00 -0500

    Add files via upload

---
+++ create-2.sql
@@ -0,0 +1,71 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2024-11-23 00:46:53.812
+
+-- tables
+-- Table: Channel
+CREATE TABLE Channel (
+    ChannelID int  NOT NULL,
+    Channel_name text  NOT NULL,
+    Permission text  NOT NULL,
+    Rank int  NOT NULL,
+    CONSTRAINT Channel_pk PRIMARY KEY (ChannelID)
+);
+
+-- Table: Contact
+CREATE TABLE Contact (
+    UserID1 int  NOT NULL,
+    UserID2 int  NOT NULL,
+    Sent_From text  NOT NULL,
+    Sent_To text  NOT NULL,
+    CONSTRAINT Contact_pk PRIMARY KEY (UserID1,UserID2)
+);
+
+-- Table: Message
+CREATE TABLE Message (
+    MessageID int  NOT NULL,
+    message text  NOT NULL,
+    CONSTRAINT Message_pk PRIMARY KEY (MessageID)
+);
+
+-- Table: Message_Details
+CREATE TABLE Message_Details (
+    MessageID int  NOT NULL,
+    UserID1 int  NOT NULL,
+    UserID2 int  NOT NULL,
+    CONSTRAINT Message_Details_pk PRIMARY KEY (MessageID,UserID1,UserID2)
+);
+
+-- Table: Userinfo
+CREATE TABLE Userinfo (
+    UserID int  NOT NULL,
+    ChannelID int  NOT NULL,
+    CONSTRAINT Userinfo_pk PRIMARY KEY (UserID,ChannelID)
+);
+
+-- foreign keys
+-- Reference: R4_R1 (table: Message_Details)
+ALTER TABLE Message_Details ADD CONSTRAINT R4_R1
+    FOREIGN KEY (MessageID)
+    REFERENCES Message (MessageID)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: R4_R3 (table: Message_Details)
+ALTER TABLE Message_Details ADD CONSTRAINT R4_R3
+    FOREIGN KEY (UserID1, UserID2)
+    REFERENCES Contact (UserID1, UserID2)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: R6_R5 (table: Userinfo)
+ALTER TABLE Userinfo ADD CONSTRAINT R6_R5
+    FOREIGN KEY (ChannelID)
+    REFERENCES Channel (ChannelID)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- End of file.
+
