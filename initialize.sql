37a3f34ed7f6afffb1d78201dbc499601d7f5193
Simon002-guo <junyug@andrew.cmu.edu>
2024-11-22 22:33:47 -0500

    Update initialize.sql

--- initialize.sql
+++ initialize.sql
@@ -7,8 +7,6 @@ CREATE database trax;
 \i create.SQL
 
 -- load the data
-\copy Message(MessageID, message) FROM data/Messages.csv csv header;
-\copy Contact(UserID1, UserID2, Sent_From, Sent_To) FROM data/Contact.csv csv header;
-\copy Message_Details(UserID1, UserID2, MessageID) FROM data/Message_Details.csv csv header;
-\copy Channel(ChannelID Channel name, Permission, Rank) FROM data/Channel.csv csv header;
-\copy Userinfo(ChannelID, UserID) FROM data/Userinfo.csv csv header;
+\copy User(UserID,Username,email) FROM data/User.csv header;
+\copy Channel(ChannelID, Channel name, Permission, Rank) FROM data/Channel.csv csv header;
+\copy Channel_Message(ChannelID, UserID,Message) FROM data/Channel_Message.csv csv header;
