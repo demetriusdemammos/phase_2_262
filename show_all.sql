fe9cc8351f3b8595636dd5dbe100aa01b024b0ab
Simon002-guo <junyug@andrew.cmu.edu>
2024-11-23 14:03:49 -0500

    Add files via upload

---
+++ show_all.sql
@@ -0,0 +1,7 @@
+\c trax
+\echo "display the User table"
+SELECT * FROM "User";
+\echo "display the Channel table"
+SELECT * FROM Channel;
+\echo "display Channel_Message table"
+SELECT * FROM Channel_Message;
