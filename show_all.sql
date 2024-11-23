80bac24ede2192e74f8683439d1e657f3975ce52
Simon002-guo <junyug@andrew.cmu.edu>
2024-11-23 14:03:14 -0500

    Delete show_all.sql

--- show_all.sql
+++
@@ -1,4 +0,0 @@
-SELECT u.UserID,u.Username,u.email,c1.Message,c1.Time,c2.ChannelID,c2.Channelname,c2.Permission,c2.Rank
-  FROM User AS u
-  JOIN Channel_Message AS c1 ON u.UserID=c1.UserID
-  JOIN Channel AS c2 ON c1.ChannelID=c2.ChannelID;
