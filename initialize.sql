d5039b141fb5786be6a2bbff049471f1edddff6b
Simon002-guo <junyug@andrew.cmu.edu>
2024-12-07 11:55:47 -0500

    Update initialize.sql

--- initialize.sql
+++ initialize.sql
@@ -11,3 +11,6 @@ CREATE database trax;
 \copy Channel(ChannelID, Channelname, Permission, Rank) FROM './Channel.csv' csv header;
 \copy Channel_Message(Message,Time,UserID,ChannelID) FROM './Channel_Message.csv' csv header;
 \copy Message(MessageID,Sent_From,Sent_To,message) FROM './Message.csv' csv header;
+\copy Common_User(UserID,"role") FROM './Common_User.csv' csv header;
+\copy Event(EventID,Event_Time) FROM './Event.csv' csv header;
+\copy Event_Participation(Time_Duration,Time_participated,UserID,EventID) FROM '.Event_Participation.csv' csv header;
